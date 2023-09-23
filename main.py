bdaa57ed72ba5d05d632e34d7699b8b61f936e9d
Mykola1601 <nik160186@gmail.com>
2023-09-24 00:21:08 +0300

    night

--- main.py
+++ main.py
@@ -1,30 +1,52 @@
 #  HW9
+import sys
 
-def gritting(_):
+
+phone_book = {
+    'Nik':'0935609516',
+    'Anna':'0993331122',
+}
+
+
+# greetings
+def greeting(_):
     print("How can I help you?")
 
+# add contact
 def add(text=""):
-    print(text)
-    print("adding")
+    words = text.split()[1:3]
+    phone_book[words[0].title()] = words[1]
 
+# change contact if exist
 def change(text=""):
-    print("chsnging")
+    words = text.split()[1:3]
+    if words[0].title() in phone_book.keys():
+        phone_book[words[0].title()] = words[1]
+    else:
+        print("no contact")
 
+# search contact 
 def phone(text=""):
-    print("phone read")
+    words = text.split()[1:3]
+    print(phone_book[words[0].title()] )
 
+# show all
 def show(text=""):
-    print("showing")
+    print(phone_book)
 
+# exit program
 def exit(_):
-    print("Good bye!")
+    # print("Good bye!")
+    sys.exit('Good bye!\n')
 
 
 dic = {
-    "hello":gritting,
+    "hello":greeting,
     "add":add,
     "change":change,
     "phone":phone,
+    "show all":show,
+    "show":show,
     "exit":exit,
     "close":exit,
     "good bye":exit,
@@ -35,16 +57,16 @@ def find_command(text=""):
     text = text.lower()
     for kee in dic.keys():
         if kee in text:
-            print("====",kee)
+            # print("====",kee)
             p = (dic[kee])
             return p(text)
-    return print("waiting")
+    return print("do not undestend")
 
+print("I'm BOT, hello!!!")
 
-
-find_command( " first seKOnd hello mister")
-
-
+while True:
+    user_input =  (input(">>>"))
+    find_command( user_input)
 
 
 
