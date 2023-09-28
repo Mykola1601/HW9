ecc206fec8bd15bb61e1036d41a0773f47a4809b
Mykola1601 <nik160186@gmail.com>
2023-09-28 20:43:43 +0300

    2809232043

--- main.py
+++ main.py
@@ -8,107 +8,122 @@ phone_book = {
     'Anna':'+380993331122',
 }
 
-def main():
-    print("I'm Phone_Book_BOT, hello!!!")
 
 
-
-    def normalize_phone(text = ''):
-        numbers = re.findall('\d+', text)
-        phone = (''.join(numbers))  
-        if   12 >= len(phone) >= 10:
-            return "+38"+phone[-10:]
-        else:
-            print('Phone number ERROR')
-            return None
-
-
-    # greetings
-    def greeting(_):
-        print("How can I help you?")
-
-    # add contact
-    def add(text=""):
-        # text = text[text.find("add"):]
-        text = text.removeprefix("add ")
-        # print (text)
-        phone = normalize_phone(text)
-        if phone :
-            words = text.split()
-            name = words[0].title()
-            phone_book[name] = phone
-            print(name+" saved with number "+ phone)
-
-    # change contact if exist
-    def change(text=""):
-        # text = text[text.find("change"):]
-        text = text.removeprefix("change ")
-        phone = normalize_phone(text)
-        if not phone: return
-        words = text.split()
-        name = words[0].title()
-        if name in phone_book.keys():
-            phone_book[name] = phone
-            print(name+" change number to "+ phone)
-        else:
-            print("no contact")
-
-    # search contact 
-    def phone(text=""):
-        text = text.removeprefix("phone ")
-        words = text.split()
-        name = words[0].title()
-        if name in phone_book.keys():
-            print(name+' -> '+phone_book[name] )
-        else:
-            print(name,' not exist in phone book!!!')
-            # name_input =  (input("Enter user name >>>"))
-            # if not find_command(name_input):
-            #     phone(text=name_input)
-
-
-    # show all
-    def show(text=""):
-        for nam, ph in phone_book.items() :
-            print(f"{nam} --> {ph} ")
-
-    # exit program
-    def exit(_):
-        # print("Good bye!")
-        sys.exit('Good bye!\n')
-
-    # dict for commands
-    dic = { 
-        "hello":greeting,
-        "add ":add,
-        "change ":change,
-        "phone ":phone,
-        "show all":show,
-        "show":show,
-        "exit":exit,
-        "close":exit,
-        "good bye":exit,
-    }
-
-    # find command in text
-    def find_command(text=""): 
-        text = text.lower()
-        for kee in dic.keys():
-            if kee in text:
-                func = dic[kee]
-                return func(text)
-        print("Do not undestend")
+def normalize_phone(text = ''):
+    numbers = re.findall('\d+', text)
+    phone = (''.join(numbers))  
+    iterator = re.finditer(r"0[\d]{9}", phone)
+    if iterator:
+        for match in iterator:  
+            phone = match.group()
+            return "+38"+phone
+    else:
         return None
 
 
+# greetings
+def greeting(_):
+    return ("How can I help you?")
 
-# loop forever
+
+# add contact
+def add(text=""):
+    text = text.removeprefix("add ")
+    name = text.split()[0].title()
+    text = text.removeprefix(name)
+    if not len(text) >9:
+        return 'Enter valid name & phone'
+    phone = normalize_phone(text)
+    if not phone:
+        return 'Enter valid phone'
+    if not name in phone_book.keys():
+        phone_book[name] = phone
+        return name+" saved with number "+ phone
+    else:
+         return name+' allready exist in phone book'
+
+# change contact if exist
+def change(text=""):
+    text = text.removeprefix("change ")
+    name = text.split()[0].title()
+    text = text.removeprefix(name)
+    if not len(text) >9:
+        return 'Enter valid name & phone'
+    phone = normalize_phone(text)
+    if not phone:
+        return 'Enter valid phone'
+    if name in phone_book.keys():
+        phone_book[name] = phone
+        return name+" change number to "+ phone
+    else:
+        return (f"no {name} in phone book")
+
+# search contact 
+def phone(text=""):
+    text = text.removeprefix("phone ")
+    name = text.split()[0].title()
+    if name in phone_book.keys():
+        return name+' -> '+phone_book[name] 
+    else:
+        return name+' not exist in phone book!!!' 
+
+
+# show all
+def show(_):
+    list = ''
+    for nam, ph in phone_book.items() :
+        list += (f"{nam} --> {ph} \n")
+    return list
+
+
+# exit program
+def exit(_):
+    # print("Good bye!")
+    return sys.exit('Good bye!\n')
+
+
+# dict for commands
+dic = { 
+    "hello":greeting,
+    "add ":add,
+    "change ":change,
+    "phone ":phone,
+    "show all":show,
+    "show":show,
+    "exit":exit,
+    "close":exit,
+    "good bye":exit,
+}
+
+
+# find command in text
+def find_command(text=""): 
+    text = text.lower()
+    for kee in dic.keys():
+        if kee in text:
+            # func = dic[kee]
+            return kee
+
+    return None
+
+
+def main():
+    print("I'm Phone_Book_BOT, HELLO!!!")
+
+    # loop forever
     while True:
         user_input =  (input(">>>"))
-        find_command( user_input)
+        comand = find_command(user_input)
+        if not comand:
+            print("Do not undestend, try again")
+        else:
+            out = dic[comand](user_input)
+            print(out)
 
 
 
 
+# ========================================
 if __name__ == "__main__":
     main()
--- test.py
+++ test.py
@@ -15,7 +15,26 @@ text = 'add Mike   + 3 8 067 123 45-67'
 # print(phone)
 
 
-text = text[text.find("add"):]
-print (text)
-text = text.removeprefix("add ")
-print (text)
+# text = text[text.find("add"):]
+# print (text)
+# text = text.removeprefix("add ")
+# print (text)
+
+text = '+3-8 0 97 (333) 44 - 22'
+numbers = re.findall('\d+', text)
+# print(numbers)
+phone = (''.join(numbers))  
+# print ( (phone))
+
+# if   12 >= len(phone) >= 10:
+#     return "+380"+phone[-9:]
+# else:
+#     # print('Phone number ERROR')
+#     return None
+
+iterator = re.finditer(r"0[\d]{9}", phone)
+if ( iterator):
+    for match in iterator:  
+        print(match.group())
+
+
